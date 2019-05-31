0ea134ff42921668cc565bcee6e491df4b6beab8
Mauricio <tecinfomauricio@gmail.com>
2019-05-31 19:11:29 -0300

    1.3 - update animation

--- res/js/jump.js
+++ res/js/jump.js
@@ -4,40 +4,40 @@ const jumper = () => {
   // private variable cache
   // no variables are created during a jump, preventing memory leaks
 
-  let element         // element to scroll to                   (node)
+  let element // element to scroll to                   (node)
 
-  let start           // where scroll starts                    (px)
-  let stop            // where scroll stops                     (px)
+  let start // where scroll starts                    (px)
+  let stop // where scroll stops                     (px)
 
-  let offset          // adjustment from the stop position      (px)
-  let easing          // easing function                        (function)
-  let a11y            // accessibility support flag             (boolean)
+  let offset // adjustment from the stop position      (px)
+  let easing // easing function                        (function)
+  let a11y // accessibility support flag             (boolean)
 
-  let distance        // distance of scroll                     (px)
-  let duration        // scroll duration                        (ms)
+  let distance // distance of scroll                     (px)
+  let duration // scroll duration                        (ms)
 
-  let timeStart       // time scroll started                    (ms)
-  let timeElapsed     // time spent scrolling thus far          (ms)
+  let timeStart // time scroll started                    (ms)
+  let timeElapsed // time spent scrolling thus far          (ms)
 
-  let next            // next scroll position                   (px)
+  let next // next scroll position                   (px)
 
-  let callback        // to call when done scrolling            (function)
+  let callback // to call when done scrolling            (function)
 
   // scroll position helper
 
-  function location () {
+  function location() {
     return window.scrollY || window.pageYOffset
   }
 
   // element offset helper
 
-  function top (element) {
+  function top(element) {
     return element.getBoundingClientRect().top + start
   }
 
   // rAF loop helper
 
-  function loop (timeCurrent) {
+  function loop(timeCurrent) {
     // store time scroll started, if not started already
     if (!timeStart) {
       timeStart = timeCurrent
@@ -53,14 +53,15 @@ const jumper = () => {
     window.scrollTo(0, next)
 
     // check progress
-    timeElapsed < duration
-      ? window.requestAnimationFrame(loop)       // continue scroll loop
-      : done()                                   // scrolling is done
+    timeElapsed < duration ?
+      window.requestAnimationFrame(loop) // continue scroll loop
+      :
+      done() // scrolling is done
   }
 
   // scroll finished helper
 
-  function done () {
+  function done() {
     // account for rAF time rounding inaccuracies
     window.scrollTo(0, start + distance)
 
@@ -84,11 +85,11 @@ const jumper = () => {
 
   // API
 
-  function jump (target, options = {}) {
+  function jump(target, options = {}) {
     // resolve options, or use defaults
     duration = options.duration || 1000
     offset = options.offset || 0
-    callback = options.callback                       // "undefined" is a suitable default, and won't be called
+    callback = options.callback // "undefined" is a suitable default, and won't be called
     easing = options.easing || easeInOutQuad
     a11y = options.a11y || false
 
@@ -99,20 +100,20 @@ const jumper = () => {
     switch (typeof target) {
       // scroll from current position
       case 'number':
-        element = undefined           // no element to scroll to
-        a11y = false                  // make sure accessibility is off
+        element = undefined // no element to scroll to
+        a11y = false // make sure accessibility is off
         stop = start + target
         break
 
-      // scroll to element (node)
-      // bounding rect is relative to the viewport
+        // scroll to element (node)
+        // bounding rect is relative to the viewport
       case 'object':
         element = target
         stop = top(element)
         break
 
-      // scroll to element (selector)
-      // bounding rect is relative to the viewport
+        // scroll to element (selector)
+        // bounding rect is relative to the viewport
       case 'string':
         element = document.querySelector(target)
         stop = top(element)
@@ -129,7 +130,7 @@ const jumper = () => {
         duration = options.duration
         break
 
-      // function passed the distance of the scroll
+        // function passed the distance of the scroll
       case 'function':
         duration = options.duration(distance)
         break
